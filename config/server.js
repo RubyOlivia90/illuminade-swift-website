2fb8fe6a06ee863d455c90a1d5ff10760c64708e
RubyOlivia90 <livlymedia@gmail.com>
2025-09-24 03:06:09 -0400

    Server issue

--- config/server.js
+++ config/server.js
@@ -1,19 +1,10 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  url: env('PUBLIC_URL', 'http://localhost:1337'),
-  settings: {
-    cors: {
-      enabled: true,
-      origin: ['http://localhost:5173', 'http://localhost:1337'],
-      headers: ['*'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+  url: env('PUBLIC_URL'), 
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
