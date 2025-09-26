3f9ad698b9fd26873428e4b3679ef8188b8b2654
RubyOlivia90 <livlymedia@gmail.com>
2025-09-25 20:49:32 -0400

    fixed server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL'), 
+  url: env('PUBLIC_URL', 'https://illuminade-swift-website.onrender.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
