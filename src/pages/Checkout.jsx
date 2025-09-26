492ab1223251600be079547b54aaa05fba0d8925
RubyOlivia90 <livlymedia@gmail.com>
2025-09-26 00:12:28 -0400

    fixing more problems with cartcontext

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useCart } from "../CartContext";
 
 export default function Checkout() {
