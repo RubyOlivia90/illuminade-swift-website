90de781b6c895169bba5d9362b32d0028c0c6274
RubyOlivia90 <livlymedia@gmail.com>
2025-09-26 01:10:50 -0400

    added cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+  // Configure the email plugin (already present)
   email: {
     config: {
       provider: 'sendgrid',
@@ -8,6 +9,23 @@ module.exports = ({ env }) => ({
       settings: {
         defaultFrom: env('SENDGRID_DEFAULT_FROM', 'info.iluminade@gmail.com'),
         defaultReplyTo: env('SENDGRID_DEFAULT_REPLY_TO', 'info.iluminade@gmail.com'),
+      },
+    },
+  },
+  
+  // ADD: Configure the upload plugin to use Cloudinary
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
