8b16b01e8aa09b3e1c7db22ccbbd4edc0ea0b9f9
RubyOlivia90 <livlymedia@gmail.com>
2025-09-25 20:57:44 -0400

    fixed it for real

--- config/server.js
+++ config/server.js
@@ -2,6 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('PUBLIC_URL', 'https://illuminade-swift-website.onrender.com'),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
