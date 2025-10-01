f59ec151f8829f63c00a41c56745d127e7220af8
RubyOlivia90 <livlymedia@gmail.com>
2025-10-01 19:10:51 -0400

    redirects file added

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://localhost:1337',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
-        },
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  }
-
-  if (command === 'build') {
-    config.base = './'
-  }
-
-  return config
+  },
 })
