9421f35207bbc4a1f3cf1d57512752024e427d82
RubyOlivia90 <livlymedia@gmail.com>
2025-10-01 22:36:56 -0400

    /

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ command }) => {
-  if (command === 'build') {
-    return {
-      base: './',   // <-- RELATIVE paths (works anywhere)
-      plugins: [react()],
-    }
-  } else {
-    return {
-      plugins: [react()],
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://localhost:1337',
-            changeOrigin: true,
-            rewrite: (path) => path.replace(/^\/api/, ''),
-          },
-        },
+export default defineConfig({
+  plugins: [react()],
+  base: './',          // relative paths so assets load anywhere
+  build: {
+    target: 'es2017',  // transpile JS so Safari/iOS can run it
+    outDir: 'dist',    // default build folder
+    assetsDir: 'assets' // keep assets organized
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:1337',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
-    }
-  }
+    },
+  },
 })
