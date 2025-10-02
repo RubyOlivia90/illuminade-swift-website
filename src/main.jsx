9f2a142f12dfae1acdb5d8f0aaae3364df2f36bf
RubyOlivia90 <livlymedia@gmail.com>
2025-10-01 20:07:29 -0400

    back to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { CartProvider } from './CartContext';
-// Switch back to BrowserRouter
-import { BrowserRouter as Router } from 'react-router-dom';
+// This is the key change: Go back to HashRouter
+import { HashRouter as Router } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Use the HashRouter here */}
     <Router>
       <CartProvider>
         <App />
